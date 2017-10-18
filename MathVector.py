10f5ad82db232f9f808a27d18d05c8c1bbe4a7c3
Glen Paul Florendo <9373317+glenpaulvf@users.noreply.github.com>
2017-10-17 21:48:21 -0700

    Convert indentation to spaces

--- MathVector.py
+++ MathVector.py
@@ -6,40 +6,40 @@ from math import sqrt
 
 class MathVector:
 
-	def __init__(self, *dimensions):
-		self.components = []
-		if len(dimensions) == 1:
-			dim = dimensions[0]
-			if type(dim) == int:
-				self.components = [0, 0, 0, 0, 0]
-			elif type(dim) == list or type(dim) == tuple:
-				for d in dim:
-					self.components.append(d)
-		else:
-			for d in dimensions:
-				self.components.append(d)
+    def __init__(self, *dimensions):
+        self.components = []
+        if len(dimensions) == 1:
+            dim = dimensions[0]
+            if type(dim) == int:
+                self.components = [0, 0, 0, 0, 0]
+            elif type(dim) == list or type(dim) == tuple:
+                for d in dim:
+                    self.components.append(d)
+        else:
+            for d in dimensions:
+                self.components.append(d)
 
-	def get_el(self, index):
-	# returns the i-th component of the vector
-		for i in range(len(self.components)):
-			if i == index - 1:
-				return self.components[i]
+    def get_el(self, index):
+    # returns the i-th component of the vector
+        for i in range(len(self.components)):
+            if i == index - 1:
+                return self.components[i]
 
-	def neg(self):
-		return MathVector([-x for x in self.components])
+    def neg(self):
+        return MathVector([-x for x in self.components])
 
-	def mag(self):
-		return sqrt(sum(map(lambda x: x*x, self.components)))
+    def mag(self):
+        return sqrt(sum(map(lambda x: x*x, self.components)))
 
-	def dot(self, other):
-		return sum([x * y for x, y in zip(self.components, other.components)])
+    def dot(self, other):
+        return sum([x * y for x, y in zip(self.components, other.components)])
 
-	def plus(self, other):
-		return MathVector([x + y for x, y in zip(self.components, other.components)])
+    def plus(self, other):
+        return MathVector([x + y for x, y in zip(self.components, other.components)])
 
-	def sp(self, scalar):
-		return MathVector([x * scalar for x in self.components])
+    def sp(self, scalar):
+        return MathVector([x * scalar for x in self.components])
 
-	def print_me(self):
-		print self.components
+    def print_me(self):
+        print self.components
 
